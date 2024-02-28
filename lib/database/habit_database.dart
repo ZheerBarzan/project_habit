50bac4740bc4169e1e96efedbb06bb7f2a50cf08
zheer barzan <zheerbarzan85@outlook.com>
2024-02-28 18:28:52 +0300

    making all the crud operatins and the providers as well

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -37,14 +37,71 @@ class HabitDatabase extends ChangeNotifier {
 // crud operations
 
 //list of habits
+  final List<Habit> currentHabits = [];
 
 //create
 
+  Future<void> addHabit(String habitName) async {
+    final newHabit = Habit()..name = habitName;
+
+    await isar.writeTxn(() => isar.habits.put(newHabit));
+
+    readHabits();
+  }
+
 //read
 
+  Future<void> readHabits() async {
+    List<Habit> fetchedHabits = await isar.habits.where().findAll();
+
+    currentHabits.clear();
+    currentHabits.addAll(fetchedHabits);
+
+    notifyListeners();
+  }
+
 //update check habit on or off
+  Future<void> updateHabitCompletion(int id, bool completed) async {
+    final habit = await isar.habits.get(id);
+
+    if (habit != null) {
+      await isar.writeTxn(() async {
+        if (completed && !habit.completedDays.contains(DateTime.now())) {
+          final today = DateTime.now();
+
+          habit.completedDays.add(DateTime(today.year, today.month, today.day));
+        } else {
+          habit.completedDays.removeWhere(
+            (date) =>
+                date.year == DateTime.now().year &&
+                date.month == DateTime.now().month &&
+                date.day == DateTime.now().day,
+          );
+        }
+
+        await isar.habits.put(habit);
+      });
+    }
+    readHabits();
+  }
 
 //update edit habit name
+  Future<void> updateHabitName(int id, String newName) async {
+    final habit = await isar.habits.get(id);
+    if (habit != null) {
+      await isar.writeTxn(() async {
+        habit.name = newName;
+        await isar.habits.put(habit);
+      });
+    }
+    readHabits();
+  }
 
 // delete habit
+  Future<void> deleteHabit(int id) async {
+    await isar.writeTxn(() async {
+      isar.habits.delete(id);
+    });
+    readHabits();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:project_habit/database/habit_database.dart';
 import 'package:project_habit/pages/hompage.dart';
 import 'package:project_habit/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HabitDatabase.initialize();
+  await HabitDatabase().saveFirstLuanchDate();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => HabitDatabase()),
+      ChangeNotifierProvider(create: (context) => ThemeProvider()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
